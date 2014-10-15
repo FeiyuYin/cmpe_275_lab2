353249a6223d4ba5ce926b62b631cf383b969cc1
divyanshi2 <divyanshi.dwivedi11@gmail.com>
2014-10-15 09:55:40 -0700

    Update ProfileService.java

--- src/main/java/edu/sjsu/cmpe275/lab2/ProfileService.java
+++ src/main/java/edu/sjsu/cmpe275/lab2/ProfileService.java
@@ -2,10 +2,28 @@ package edu.sjsu.cmpe275.lab2;
 
 import java.util.HashMap;
 
-public interface ProfileService {
+/**
+ * ProfileService is an interface which is implemented in ProfileServiceImpl class
+ */
 
-	public HashMap<String, Profile> getHashmap();
-	public Profile getProfile(String userId);
-	public void storeProfile(Profile pro);
+public interface ProfileService {
+	
+	
+
+	public HashMap<String, Profile> getHashmap(); //stores objects of profile
+	
+	/**
+	 *  Retrieve profile
+	 *  @param userId the ID of user whose profile we want to 
+	 * @reurn Profile of the user with given ID
+	 */
+	public Profile getProfile(String userId); 
+	
+	/**
+	 * Store complete Profile 
+	 * @param Profile object whose Profile is to be stored
+	 * @retun void
+	 */
+	public void storeProfile(Profile pro);        // Stores complete profile @param Profile object
 	
 }
